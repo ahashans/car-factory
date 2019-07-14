08a2677d5cc7b6c9a093870d7a3df2157296d5ce
ahashans <ahashans@gmail.com>
2019-07-14 23:21:41 +0600

    Initial Commit

---
+++ IVehicleBuilder.cs
@@ -0,0 +1,9 @@
+public interface IVehicleBuilder
+{
+    void SetModel();
+    void SetEngine();
+    void SetTransmission();
+    void SetBody();
+    void SetAccessories();
+    Vehicle GetVehicle();
+}
